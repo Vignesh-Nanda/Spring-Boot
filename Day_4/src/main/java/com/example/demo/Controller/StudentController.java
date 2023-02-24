ac9fc4fc9841fb4d605515182b3905413de6a3b0
Vignesh N <100300351+Vignesh-Nanda@users.noreply.github.com>
2023-02-24 11:04:14 +0530

    Add files via upload

---
+++ Day_4/src/main/java/com/example/demo/Controller/StudentController.java
@@ -0,0 +1,55 @@
+package com.example.demo.Controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Entity.Student;
+import com.example.demo.Repository.StudentRepo;
+import com.example.demo.Service.StudentService;
+@RestController
+public class StudentController {
+	
+	@Autowired
+	private StudentService stuservice;
+	@GetMapping("/getstudent")
+	public List<Student>getDetails()
+	{
+		return stuservice.getAllDetails();
+	}
+	
+	@PostMapping("/addstudent")
+	public Student addDetails(@RequestBody Student s) 
+	{
+		return stuservice.addDetails(s);
+	}
+		@PutMapping("/put{id}")
+	
+	
+	@DeleteMapping("/del/{id}")
+	public void delDetails(@PathVariable("id") Long s) 
+	{
+		stuservice.deleteDepartmentId(s);
+	}
+		
+		@GetMapping("/getstudent/{id}")
+		public Optional<Student> getDetails(@PathVariable("id") Long id)
+		{
+			return stuservice.getOne(id);
+		}
+//		
+//		@PutMapping("/put/{id}")
+//		public Student updateUser(@PathVariable("id") Long id, @RequestBody Student s)
+//		{
+//			Student update=StudentRepo.findById(id);
+//		}
+
+}
